83efee9806e83fefbacce171c62abe0feb6bd568
sueanyv <valenciasue@hotmail.com>
2016-11-16 20:04:06 -0800

    changes

---
+++ scripts/repos.js
@@ -0,0 +1,19 @@
+var repo = [{
+      title: 'about_me',
+      projectUrl: 'https://sueanyv.github.io/about_me/',
+      imagePath: '',
+  },
+
+  {
+    title: 'cookie-stand',
+    projectUrl: 'https://sueanyv.github.io/cookie-stand/',
+    imagePath: '',
+
+  },
+
+  {
+    title: 'bus_mall',
+    projectUrl: 'https://sueanyv.github.io/bus_mall/',
+    imagePath: '',
+  },
+];
